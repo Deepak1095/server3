ff7be72c6b4cd454b4889d868cba0a72a759f839
Deepak1095 <ddhankhar999@gmail.com>
2023-08-24 10:07:08 +0530

    add cors

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const cors=require("cors")
+const cors = require("cors");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+const port = process.env.PORT || 3001;
 
-server.use(cors())
+const corsOptions = {
+  origin: 'http://localhost:4200', // Update with your Angular app's URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true // Allow credentials
+};
+
+server.use(cors(corsOptions));
+server.options("*", cors()); // Respond to OPTIONS requests
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
